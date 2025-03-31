51b4166b79f2070f4191ac5073688b9bb76c7175
Edmar-Chiullo <ecschiullo@gmail.com>
2025-03-31 14:10:50 -0300

    Class Order Removida

--- app/class/Order.ts
+++ app/class/Order.ts
@@ -3,32 +3,32 @@ import { fullDate } from "../utils/create-date";
 import { Employee } from "./classes";
 
 
-export class Order {
-    orderId: string | null
-    orderEmployeeId: number | null
-    orderEmployeeName: string | null
-    orderCliCOD: number | null
-    orderValue: string | null
-    OrderFantasia: string | null
-    orderDate: Date | null
-    orderStatus: string
+// export class Order {
+//     orderId: string | null
+//     orderEmployeeId: number | null
+//     orderEmployeeName: string | null
+//     orderCliCOD: number | null
+//     orderValue: string | null
+//     OrderFantasia: string | null
+//     orderDate: Date | null
+//     orderStatus: string
 
-    constructor(employee: Employee, client: ClientProps) {
-        this.orderId = fullDate()
-        this.orderEmployeeId = employee.employeeId
-        this.orderEmployeeName = employee?.employeePermitionType
-        this.orderCliCOD = client.cliCOD
-        this.orderValue = null
-        this.OrderFantasia = client.fantasia
-        this.orderDate = new Date()
-        this.orderStatus = ''
-    }
+//     constructor(employee: Employee, client: ClientProps) {
+//         this.orderId = fullDate()
+//         this.orderEmployeeId = employee.employeeId
+//         this.orderEmployeeName = employee?.employeePermitionType
+//         this.orderCliCOD = client.cliCOD
+//         this.orderValue = null
+//         this.OrderFantasia = client.fantasia
+//         this.orderDate = new Date()
+//         this.orderStatus = ''
+//     }
 
-    status({ status }: { status: string; }): void {
-        this.orderStatus = status
-    }
+//     status({ status }: { status: string; }): void {
+//         this.orderStatus = status
+//     }
 
-    value({ value }: {value: string}): void {
-        this.orderValue = value
-    }
-}
+//     value({ value }: {value: string}): void {
+//         this.orderValue = value
+//     }
+// }

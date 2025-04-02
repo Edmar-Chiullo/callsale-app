91dd235981d64ff7922326b13ee62185467a9881
Edmar-Chiullo <ecschiullo@gmail.com>
2025-04-02 11:52:06 -0300

    Bloqueio do EsLint no Build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Desativa o ESLint no build
+  },
 };
 
 export default nextConfig;
